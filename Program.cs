5b73b1986acc8c048e8d76d971367d980ebae93c
Nikita <nikitya39@gmail.com>
2022-05-23 02:02:43 +0300

    code

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array = { "1234", "156", "-2", "computer science", "08", "a", "test" };
+
+string[] newarray = new string[array.Length];
+
+int index = 0;
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newarray[index] = array[i];
+        index++;
+        count++;
+    }
+
+}
+
+Array.Resize(ref newarray, count);
+
+for (int i = 0; i < newarray.Length; i++)
+{
+    Console.WriteLine(newarray[i]);
+}
